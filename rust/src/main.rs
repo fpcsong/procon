f01e4fedac8296ee24902dc1eee22607a70fa313
vain0x <vainzerox@gmail.com>
2018-05-28 02:11:57 +0900

    wip!

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,115 +1,104 @@
-#![allow(unused_imports)]
-#![allow(non_snake_case)]
+use std::cmp::max;
+use std::io::BufRead;
 
-use std::cmp::{max, min, Ordering};
-use std::collections::*;
-use std::io::*;
-use std::ops::*;
-use std::*;
+fn diff(s: &[char], t: &[char]) -> Vec<(char, bool)> {
+    let sn = s.len();
+    let tn = t.len();
 
-// -----------------------------------------------
-// Framework
-// -----------------------------------------------
+    // dp[i][j] = k <=> s[..i], t[..j] の最長部分共通列の末尾が s[k - 1]
+    let mut dp = vec![vec![0; tn + 1]; sn + 1];
 
-#[allow(unused)]
-fn rl() -> String {
-    let mut buf = String::new();
-    io::stdin().read_line(&mut buf).unwrap();
-    buf.trim_right().to_owned()
-}
+    for si in 0..sn + 1 {
+        for ti in 0..tn + 1 {
+            if si < sn && ti < tn && s[si] == t[ti] {
+                dp[si + 1][ti + 1] = si + 1;
+            }
 
-#[allow(unused)]
-fn rw<T>() -> Vec<T>
-where
-    T: std::str::FromStr,
-    T::Err: std::fmt::Debug,
-{
-    let mut buf = String::new();
-    io::stdin().read_line(&mut buf).unwrap();
-    buf.split_whitespace()
-        .map(|word| T::from_str(word).unwrap())
-        .collect()
-}
+            if si + 1 < sn + 1 {
+                dp[si + 1][ti] = max(dp[si + 1][ti], dp[si][ti]);
+            }
 
-trait IteratorExt: Iterator + Sized {
-    fn vec(self) -> Vec<Self::Item> {
-        self.collect()
-    }
-}
-
-impl<T: Iterator> IteratorExt for T {}
-
-#[allow(unused)]
-macro_rules! debug {
-    ($($arg:expr),*) => {
-        let entries = &[
-            $((
-                &stringify!($arg).to_string() as &fmt::Debug,
-                &($arg) as &fmt::Debug,
-            )),*
-        ];
-        eprintln!("{:?}", DebugMap(entries));
-    };
-}
-
-#[allow(unused)]
-struct DebugMap<'a>(&'a [(&'a fmt::Debug, &'a fmt::Debug)]);
-
-impl<'a> std::fmt::Debug for DebugMap<'a> {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        let mut m = fmt.debug_map();
-        for &(key, value) in self.0.iter() {
-            m.entry(key, value);
+            if ti + 1 < tn + 1 {
+                dp[si][ti + 1] = max(dp[si][ti + 1], dp[si][ti]);
+            }
         }
-        m.finish()
     }
-}
 
-// -----------------------------------------------
-// Polyfill
-// -----------------------------------------------
+    let mut buf = Vec::new();
+    {
+        let mut si = sn;
+        let mut ti = tn;
+        while !(si == 0 && ti == 0) {
+            let k = dp[si][ti];
 
-#[derive(PartialEq, Eq, Clone, Debug)]
-pub struct Rev<T>(pub T);
-
-impl<T: PartialOrd> PartialOrd for Rev<T> {
-    fn partial_cmp(&self, other: &Rev<T>) -> Option<Ordering> {
-        other.0.partial_cmp(&self.0)
+            if si > 0 && ti > 0 && s[si - 1] == t[ti - 1] {
+                buf.push((s[si - 1], true));
+                si -= 1;
+                ti -= 1;
+            } else if si > 0 && k == dp[si - 1][ti] {
+                buf.push((s[si - 1], false));
+                si -= 1;
+            } else if ti > 0 && k == dp[si][ti - 1] {
+                buf.push((t[ti - 1], false));
+                ti -= 1;
+            } else {
+                unreachable!()
+            }
+        }
     }
+
+    buf.into_iter().rev().collect::<Vec<_>>()
 }
 
-impl<T: Ord> Ord for Rev<T> {
-    fn cmp(&self, other: &Rev<T>) -> Ordering {
-        other.0.cmp(&self.0)
+fn pattern_from_edit(edit: &[(char, bool)]) -> Vec<char> {
+    let mut pattern = Vec::new();
+    {
+        let mut prev = true;
+        for &(c, common) in edit {
+            if common && !prev {
+                pattern.push('*');
+            }
+
+            if common {
+                pattern.push(c);
+            }
+
+            prev = common;
+        }
+
+        if !prev {
+            pattern.push('*');
+        }
     }
+    pattern
 }
 
-#[allow(unused)]
-macro_rules! eprintln {
-    ($($arg:expr),*) => { _eprintln(format_args!($($arg),*)) }
-}
+fn normalize(pattern: &[char]) -> String {
+    let mut buf = Vec::new();
+    let mut prev = None;
+    for &c in pattern {
+        if c == '*' && Some(c) == prev {
+            continue;
+        }
 
-fn _eprintln(args: fmt::Arguments) {
-    let err = std::io::stderr();
-    let mut err = err.lock();
-    err.write_fmt(args).unwrap();
-    err.write(b"\n").unwrap();
+        buf.push(c);
+        prev = Some(c);
+    }
+    buf.into_iter().collect::<String>()
 }
-
-// -----------------------------------------------
-// Solution
-// -----------------------------------------------
 
 pub fn main() {
-    return;
-}
+    let stdin = std::io::stdin();
+    let lock = stdin.lock();
+    let ss = lock.lines()
+        .map(|line| line.unwrap().chars().collect::<Vec<char>>())
+        .collect::<Vec<_>>();
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_ok() {
-        assert_eq!(7, 1 + 2 * 3);
+    let mut pattern = ss[0].clone();
+    for r in ss.into_iter().skip(1) {
+        let edit = diff(&pattern, &r);
+        pattern = pattern_from_edit(&edit);
     }
+
+    println!("{}", normalize(&pattern));
 }
