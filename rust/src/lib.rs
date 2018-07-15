c34f22cee7cea0c6a8fb9a017b2fd345823d564f
vain0x <vainzerox@gmail.com>
2018-07-15 23:53:32 +0900

    wip!

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,6 @@
+#![feature(plugin)]
+#![plugin(cargo_snippet)]
+
 pub mod procon;
 
 // Stub of tests mod.
@@ -11,4 +14,40 @@ mod tests {
     fn test_ok() {
         assert_eq!(7, 1 + 2 * 3);
     }
+}
+
+// Annotate snippet name
+#[snippet = "mymath"]
+#[snippet = "gcd"]
+fn gcd(a: u64, b: u64) -> u64 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+// Also works
+#[snippet(name = "mymath")]
+// Equivalent to #[snippet = "lcm"]
+#[snippet]
+fn lcm(a: u64, b: u64) -> u64 {
+    a / gcd(a, b) * b
+}
+
+#[snippet]
+// Include snippet
+#[snippet(include = "gcd")]
+fn gcd_list(list: &[u64]) -> u64 {
+    list.iter().fold(list[0], |a, b| gcd(a, *b))
+}
+
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(57, 3), 3);
+}
+
+#[test]
+fn test_lcm() {
+    assert_eq!(lcm(3, 19), 57);
 }
