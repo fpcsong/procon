ad9b18b68f1ac10e97b9badd1e6802cfb00cc6c4
vain0x <vainzerox@gmail.com>
2018-07-28 01:21:58 +0900

    Less verbose

--- fsharp/Lib/ImperativeBuilder.fs
+++ fsharp/Lib/ImperativeBuilder.fs
@@ -8,80 +8,43 @@ namespace global
     | Ret of 'T
 
   type ImperativeBuilder private () =
-    member __.Delay(f: unit -> Imp<'r>): unit -> Imp<'r> =
-      f
+    member __.Delay(f: unit -> Imp<_>) = f
 
-    member __.Run(f: unit -> Imp<unit>): unit =
-      f () |> ignore
-
-    member __.Run(f: unit -> Imp<'r>): 'r =
+    member __.Run(f: unit -> Imp<unit>) = f () |> ignore
+    member __.Run(f: unit -> Imp<_>) =
       match f () with
-      | Ret x ->
-        x
-      | Step
-      | Break
-      | Next ->
-        InvalidProgramException() |> raise
+      | Ret x -> x
+      | Step | Break | Next -> failwith "Never"
 
-    member __.Zero(): Imp<'r> =
-      Step
+    member __.Zero() = Step
+    member __.Return(r) = Ret r
+    member __.ReturnFrom(m: Imp<_>) = m
 
-    member __.Return(r: 'r): Imp<'r> =
-      Ret r
-
-    member __.ReturnFrom(m: Imp<'r>): Imp<'r> =
-      m
-
-    member __.Bind(m: Imp<_>, f: unit -> Imp<_>): Imp<_> =
+    member __.Bind(m: Imp<_>, f) =
       match m with
-      | Step ->
-        f ()
-      | Break
-      | Next
-      | Ret _ ->
-        m
+      | Step                  -> f ()
+      | Break | Next | Ret _  -> m
 
-    member __.Combine(r: Imp<'r>, f: unit -> Imp<'r>): Imp<'r> =
+    member __.Combine(r: Imp<_>, f) =
       match r with
-      | Step ->
-        f ()
-      | Break
-      | Next
-      | Ret _ ->
-        r
+      | Step                  -> f ()
+      | Break | Next | Ret _  -> r
 
-    member __.Using(x: 'r, f: 'r -> Imp<'y>): Imp<'y> =
-      use _x = x
-      f x
+    member __.Using(x, f): Imp<_> = using x f
+    member __.TryWith(f, h) = try f () with e -> h e
+    member __.TryFinally(f, g) = try f () finally g ()
 
-    member __.TryWith(f: unit -> 'r, h: exn -> 'r) =
-      try
-        f ()
-      with
-      | e -> h e
-
-    member __.TryFinally(f: unit -> 'r, g: unit -> unit) =
-      try
-        f ()
-      finally
-        g ()
-
-    member __.While(p: unit -> bool, f: unit -> Imp<'r>): Imp<'r> =
+    member __.While(p, f): Imp<_> =
       let rec loop () =
         if p () then
           match f () with
-          | Step
-          | Next ->
-            loop ()
-          | Break ->
-            Step
-          | Ret _ as r ->
-            r
-        else
-          Step
+          | Step | Next   -> loop ()
+          | Break         -> Step
+          | Ret _ as r    -> r
+        else Step
       loop ()
 
-    member this.For(xs: #seq<'x>, body: 'x -> Imp<'r>): Imp<'r> =
+    member this.For(xs: #seq<_>, body) =
       use e = xs.GetEnumerator()
       this.While(e.MoveNext, fun () -> body e.Current)
 
