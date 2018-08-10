7994cddf020af05ab3209e5109091272798de07a
vain0x <vainzerox@gmail.com>
2018-08-10 23:24:01 +0900

    wip!

--- fsharp/Solver/Program.fs
+++ fsharp/Solver/Program.fs
@@ -1,13 +1,30 @@
-﻿namespace global
-  open System
-  open System.Collections
-  open System.Collections.Generic
+﻿module Program
 
-  [<AutoOpen>]
-  module Operators =
-    let read f = Console.ReadLine().Split([|' '|]) |> Array.map f
+open BenchmarkDotNet
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Configs
+open BenchmarkDotNet.Jobs
 
-  module Program =
-    [<EntryPoint>]
-    let main _ =
-      0
+type Benchmarks() =
+  [<Benchmark>]
+  member this.IsPrimeBruteForceBench() =
+    ()
+
+  [<Benchmark>]
+  member this.IsPrimeBench() =
+    ()
+
+[<EntryPoint>]
+let main _ =
+  let config =
+    let rough = AccuracyMode(MaxRelativeError = 0.1)
+    let quickRoughJob = Job("QuickRough", rough, RunMode.Short)
+
+    let c = Configs.ManualConfig()
+    c.Add(quickRoughJob)
+
+    // その他の設定をデフォルトから継承する。
+    ManualConfig.Union(DefaultConfig.Instance, c)
+
+  let _summary = Running.BenchmarkRunner.Run<Benchmarks>(config)
+  0
